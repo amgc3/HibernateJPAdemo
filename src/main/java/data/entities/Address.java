e3acd09f7ec16a9fdf64ccd09ec9998dc10a5966
Anna Cotugno <annacotugno@Annas-MacBook-Pro.local>
2022-05-20 22:55:35 +0100

    add address

---
+++ src/main/java/data/entities/Address.java
@@ -0,0 +1,50 @@
+package data.entities;
+
+public class Address {
+
+    private String addressLine1;
+    private String getAddressLine2;
+    private String city;
+    private String postalCode;
+    private String country;
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getGetAddressLine2() {
+        return getAddressLine2;
+    }
+
+    public void setGetAddressLine2(String getAddressLine2) {
+        this.getAddressLine2 = getAddressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
