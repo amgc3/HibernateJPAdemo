09c720d84549e6d33cc12918fb3ddb3890e07a10
Anna Cotugno <annacotugno@Annas-MacBook-Pro.local>
2022-05-20 22:56:39 +0100

    simple entity mapping

--- src/main/java/data/entities/User.java
+++ src/main/java/data/entities/User.java
@@ -1,16 +1,53 @@
 package data.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
 
+@Entity
+@Table(name="FINANCES_USER")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "USER_ID")
     private Long userId;
+
+    @Column(name = "FIRST_NAME")
     private String firstName;
+
+    @Column(name = "LAST_NAME")
     private String lastName;
+
+    @Column(name="ADDRESS")
+    private Address address;
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Column(name = "BIRTH_DATE")
     private Date birthDate;
+
+    @Column(name = "EMAIL_ADDRESS")
     private String emailAddress;
+
+    @Column(name = "LAST_UPDATED_DATE")
     private Date lastUpdatedDate;
+
+    @Column(name = "LAST_UPDATED_BY")
     private String lastUpdatedBy;
     private Date createdDate;
+
+    @Column(name = "CREATED_BY")
     private String createdBy;
 
     public Long getUserId() {
