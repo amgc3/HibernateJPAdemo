8c2c5bfbd7016fc89474040636549ed8435144f6
amgc3 <anna.cotugno@deltacapita.com>
2022-05-23 20:36:29 +0100

    save user to database with autogenerated id

--- src/main/java/data/Application.java
+++ src/main/java/data/Application.java
@@ -1,12 +1,31 @@
 package data;
 
+import data.entities.User;
 import org.hibernate.Session;
+
+import java.util.Date;
 
 public class Application {
 
     public static void main(String[] args) {
         Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
+        session.getTransaction().begin();
+
+        User user = new User();
+        //user.setUserId(Long.valueOf(2));
+        user.setFirstName("Anna");
+        user.setLastName("Piva");
+        user.setBirthDate(new Date());
+        user.setEmailAddress("ap386@yahoo.com");
+        user.setLastUpdatedBy("manna");
+        user.setLastUpdatedDate(new Date());
+        user.setCreatedBy("manna");
+        user.setCreatedDate(new Date());
+
+        session.save(user);
+
+        session.getTransaction().commit();
+
         session.close();
 
     }
